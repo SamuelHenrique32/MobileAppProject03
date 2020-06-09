634522c1291b92c30d882880d01ad2f6c563cacd
SamuelHenrique32 <shdalmas@ucs.br>
2020-06-08 23:22:50 -0300

    WIP - Navigation Drawer - Fragments

---
+++ app/src/main/java/com/ucs/mobileappproject03/FragmentSecond.java
@@ -0,0 +1,6 @@
+package com.ucs.mobileappproject03;
+
+import androidx.fragment.app.Fragment;
+
+public class FragmentSecond extends Fragment {
+}
--- app/src/main/java/com/ucs/mobileappproject03/MainActivity.java
+++ app/src/main/java/com/ucs/mobileappproject03/MainActivity.java
@@ -1,20 +1,26 @@
 package com.ucs.mobileappproject03;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.google.android.material.navigation.NavigationView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
 
     DrawerLayout drawerLayout;
     ActionBarDrawerToggle actionBarDrawerToggle;
     Toolbar toolbar;
     NavigationView navigationView;
+    FragmentManager fragmentManager;
+    FragmentTransaction fragmentTransaction;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +33,7 @@ public class MainActivity extends AppCompatActivity {
 
         drawerLayout = findViewById(R.id.drawer);
         navigationView = findViewById(R.id.navigationView);
+        navigationView.setNavigationItemSelectedListener(this);
 
         actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout,toolbar, (R.string.open), (R.string.close));
 
@@ -35,5 +42,27 @@ public class MainActivity extends AppCompatActivity {
         actionBarDrawerToggle.setDrawerIndicatorEnabled(true);
 
         actionBarDrawerToggle.syncState();
+
+        fragmentManager = getSupportFragmentManager();
+
+        fragmentTransaction = fragmentManager.beginTransaction();
+
+        fragmentTransaction.add(R.id.container_fragment, new MainFragment());
+
+        fragmentTransaction.commit();
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+        if(menuItem.getItemId() == R.id.home){
+
+        }
+
+        if(menuItem.getItemId() == R.id.home){
+
+        }
+
+        return true;
     }
 }
